998b1a7b3571b8b015809157e103dfe12345bac6
Surmiya K S <surmiyashanavas5599@gmail.com>
2026-04-02 12:14:40 +0530

    Create smallest_even_multiple.py

---
+++ Day3/smallest_even_multiple.py
@@ -0,0 +1,7 @@
+def smallestEvenMultiple(n:int)
+        if n%2==0:
+            result=n
+        else:
+            result=n*2
+        return result
+        print("The smallest multiple of both",n,"and 2 is",result)
